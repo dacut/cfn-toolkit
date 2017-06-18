0e56439024213ea13a450aefe6941093262a172e
David Cuthbert <dacut@kanga.org>
2017-06-18 12:11:04 -0700

    Remove binary support when deleted.

--- handler.py
+++ handler.py
@@ -82,20 +82,25 @@ def api_gateway_binary(event):
         Properties:
           RestApiId: !Ref RestApi
     """
-    if event["RequestType"] not in ("Create", "Update"):
-        return
-
     apigw = boto3.client("apigateway")
+    request_type = event["RequestType"]
     rest_api_id = event["ResourceProperties"]["RestApiId"]
 
     # Do we already have binary support enabled?
     rest_api_info = apigw.get_rest_api(restApiId=rest_api_id)
+    binary_enabled = ("binaryMediaTypes" in rest_api_info and
+                      "*/*" in rest_api_info["binaryMediaTypes"])
 
-    if ("binaryMediaTypes" not in rest_api_info or
-        "*/*" not in rest_api_info["binaryMediaTypes"]):
-        apigw.update_rest_api(restApiId=rest_api_id, patchOperations=[
-            {"op": "add", "path": "/binaryMediaTypes/*~1*"}
-        ])
+    if request_type in ("Create", "Update"):
+        if not binary_enabled:
+            apigw.update_rest_api(restApiId=rest_api_id, patchOperations=[
+                {"op": "add", "path": "/binaryMediaTypes/*~1*"}
+            ])
+    elif request_type == "Delete":
+        if binary_enabled:
+            apigw.update_rest_api(restApiId=rest_api_id, patchOperations=[
+                {"op": "remove", "path": "/binaryMediaTypes/*~1*"}
+            ])
 
     return
 
